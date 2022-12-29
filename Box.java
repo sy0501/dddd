688be6184cfe8caa0da53c2654d529740010d5ad
sy0501 <se980501@gmail.com>
2022-12-29 09:14:03 +0900

    first commit

---
+++ Box.java
@@ -0,0 +1,16 @@
+
+public class Box {
+
+	int width;
+	int length;
+	int height;
+	
+	//Box 객체의 가로, 세로, 높이를 20, 20, 30으로 설정하고 상자에 대한 정보를 아래와 같이 출력
+	//출력 내용
+	// 상자의 가로, 세로, 높이 값은 20, 20, 30입니다.
+	
+	void BoxInfo() {
+		System.out.printf("상자의 가로, 세로, 높이 값은 %d, %d, %d입니다", width, length, height);
+	}
+	
+}
